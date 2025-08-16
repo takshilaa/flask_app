32a70acde30ca03cf3ceb9367b6895828166345a
takshilaa <takshil08@gmail.com>
2025-08-17 01:04:49 +0530

    Updated homepage message

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask is working on Ubuntu!"
+    return "I’m not superstitious, but I am a little stitious.!"
 
 if __name__ == "__main__":
     app.run(debug=True)
